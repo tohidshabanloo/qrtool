4bc75817fab3f2d95fd697d013f438a5cfe5ac6b
tohidshabanloo <tshabanloo@hotmail.com>
2025-11-03 20:03:33 +0330

    sitemap added 3

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -6,6 +6,20 @@ const config = {
   changefreq: 'weekly',
   priority: 0.7,
   exclude: [],
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+      { userAgent: 'Amazonbot', disallow: '/' },
+      { userAgent: 'Applebot-Extended', disallow: '/' },
+      { userAgent: 'Bytespider', disallow: '/' },
+      { userAgent: 'CCBot', disallow: '/' },
+      { userAgent: 'ClaudeBot', disallow: '/' },
+      { userAgent: 'Google-Extended', disallow: '/' },
+      { userAgent: 'GPTBot', disallow: '/' },
+      { userAgent: 'meta-externalagent', disallow: '/' },
+    ],
+    additionalSitemaps: ['https://qrtool.ir/sitemap.xml'],
+  },
   transform: async (config, path) => {
     return {
       loc: path,
